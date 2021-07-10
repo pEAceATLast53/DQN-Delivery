bf14ec72abcb4f6c69a4f41d896260aad0a0b65a
pEAceATLast53 <jekim0503@gmail.com>
2021-07-10 16:32:24 +0900

    Changed batch size : 5120 -> 1024 -> 512, 512 seems to train better and faster

--- arguments.py
+++ arguments.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser("DQN Delivery")
 parser.add_argument('--map_name', default='dist_test.png')
 
 parser.add_argument('--lidar_range', default=7)
-parser.add_argument('--num_landmarks', default=2)
+parser.add_argument('--num_landmarks', default=3)
 
 parser.add_argument('--success_reward', default=1)
 parser.add_argument('--collision_penalty', default=0.1)
@@ -18,12 +18,12 @@ parser.add_argument("--save_interval", default=10000)
 
 parser.add_argument('--device', default='cuda')
 
-parser.add_argument('--model_name', default='2_100', type=str)
+parser.add_argument('--model_name', default='3_100', type=str)
 
 # RL hyperparameters
 parser.add_argument('--target_update_interval', default=200, type=int)  
 parser.add_argument('--update_interval', default=100) 
-parser.add_argument('--batch_size', default=1024, type=int)
+parser.add_argument('--batch_size', default=512, type=int)
 parser.add_argument('--buffer_size', default=1000000, type=int)
 parser.add_argument('--init_epsilon', default=1.0, type=float)
 parser.add_argument('--final_epsilon', default=0.05, type=float)
